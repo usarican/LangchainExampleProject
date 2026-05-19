c124cb635dce40168ee2c9a6997c18b03f8a56c2
usarican <iu.sarican@gmail.com>
2026-05-19 08:56:02 +0300

    Gemini Example

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
 
 load_dotenv()  # loads GOOGLE_API_KEY from .env
 
-llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
-response = llm.invoke("Hello, Gemini!")
-print(response.content)
+
+def get_weather(city: str) -> str:
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="google_genai:gemini-2.5-flash-lite",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content_blocks)
