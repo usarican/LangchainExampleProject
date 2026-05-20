8a3420e4743e6a98fd9e0ea8e1a58cfeaf0a05db
usarican <iu.sarican@gmail.com>
2026-05-20 07:58:08 +0300

    OpenAI Example

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 
-load_dotenv()  # loads GOOGLE_API_KEY from .env
-
+load_dotenv()
 
 def get_weather(city: str) -> str:
+    """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
 agent = create_agent(
-    model="google_genai:gemini-2.5-flash-lite",
+    model="openai:gpt-5.4",
     tools=[get_weather],
     system_prompt="You are a helpful assistant",
 )
